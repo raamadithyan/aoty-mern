e01c41b950d16aa0aef04ecbfb32c62866fbec49
raamadithyan <chitha.don@gmail.com>
2023-11-06 17:05:13 +0530

    react attached to server(nodejs) using webpack

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const { MongoClient } = require("mongodb");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./view");
+app.use(express.static("public"));
 let db;
 
 app.get("/", async (req, res) => {
 	const query = await db.collection("animals").find().toArray();
 
 	// res.send(`<button>${query.map((ani) => ani.name)}</button>`);
-	res.render("home");
+	res.render("home", { query });
+});
+
+app.get("/admin", (req, res) => {
+	res.render("admin");
 });
 
 async function start() {
